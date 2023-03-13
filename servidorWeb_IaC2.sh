0a17f773f5fd58604cd2cf0f033d1fe714c4e92f
sidneyojr <sidney.o@aluno.ifsp.edu.br>
2023-03-13 01:43:33 +0000

    Adicionado Script

---
+++ servidorWeb_IaC2.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+echo "Atualização do Servidor "
+
+apt-get update
+apt-get upgrade
+apt-get install -y apache2 \
+		   unzip \
+		   wget
+
+systemctl enable apache2
+systemctl reload apache2
+ufw allow in "Apache"
+
+echo ""
+echo "Baixando e Copiando os arquivos da Aplicação..."
+cd /tmp 
+wget https://github.com/sidneyojr/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd  linux-site-dio-main
+cp -R * /var/www/html/
+
+rm /tmp/main.zip
+rm -rf /tmp/linux-site-dio-main/
+
+echo ""
+echo "Finalizado!!!!"
